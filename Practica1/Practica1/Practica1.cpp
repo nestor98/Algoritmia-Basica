401f9d050fb482077462c75d04fd4b81d2acfe53
nestor <nestormg98@gmail.com>
2020-02-25 22:23:56 +0100

    Ah y lo que habia hecho en clase

--- Practica1/Practica1/Practica1.cpp
+++ Practica1/Practica1/Practica1.cpp
@@ -3,20 +3,92 @@
 
 #include <iostream>
 #include <vector>
+
+#define CUENTA_INICIAL {-1,0,0,0,0,0,0,0,0,0}
+
+const int N_ELEMENTOS = 100;
+
 using namespace std;
+
+void inicializar(vector<string>& v, const int n) {
+    for (int i = 0; i < N_ELEMENTOS; i++) {
+        // TODO: strings aleatorias de long n
+    }
+}
+
+// Suma cada elemento i con el i+1, para i=0..9
+void acumular(vector<int>& cuenta) {
+    for (int i = 1; i < 10; i++) {
+        cuenta[i] += cuenta[i - 1];
+    }
+}
+
+
+// v el vector de numeros (formato string), n_dig el numero de digitos
+// ordena v con el algoritmo de radix LSF
+void radix(vector<string>& v, const int n_dig) {
+    // Iterar desde el digito menos significativo
+    vector<int> cuenta = CUENTA_INICIAL; // lleva la cuenta del numero de veces que aparece cada digito
+    vector<string> aux(v.size()); // auxiliar del mismo tamaño que el original
+    for (int i = n_dig - 1; i >= 0; i--) {
+        // Para cada digito de los numeros en v:
+        for (auto e : v) {
+            int digito = e[i] - '0'; // i-ésimo digito
+            cuenta[digito]++; // Contamos el digito correspondiente
+        }
+        // Acumulamos las cuentas de los digitos de izquierda a dcha:
+        acumular(cuenta);
+        // Y recolocamos cada elemento del original en el auxiliar según la cuenta:
+        for (int j = N_ELEMENTOS - 1; i >= 0; i--) { // (empezando por la dcha)
+            string elemento = v[j]; // Tomamos el j-esimo elemento
+            int digito = elemento[i] - '0'; // Y su i-esimo digito 
+            int indice = cuenta[digito]--; // Posicion en el auxiliar (y reducimos esa cuenta)
+            aux[indice] = elemento;
+        }
+        // Finalmente, actualizamos el vector original:
+        v = aux; // Comprobar que esto funcione, tengo mis dudas
+        // Y resetear la cuenta:
+        cuenta = CUENTA_INICIAL;
+    }    
+}
+
+// v el vector de numeros (formato string), n el numero de digitos
+// ordena v con el algoritmo de radix LSF
+void radix_rec(vector<string>& v, const int n, const int j) {
+    // Iterar desde el digito menos significativo
+    vector<int> cuenta = CUENTA_INICIAL; // lleva la cuenta del numero de veces que aparece cada digito
+    for (int i = n - 1; i >= 0; i--) {
+        // Para cada digito de los numeros en v:
+        for (auto e : v) {
+            int digito = e[i] - '0'; // i-ésimo digito
+            cuenta[digito]++; // Contamos el digito correspondiente
+
+        }
+        cuenta = CUENTA_INICIAL;
+    }
+
+}
+
+
+void mostrar(vector<string>& v) {
+    for (auto e : v) {
+        cout << e << " ";
+    }
+    cout << endl;
+}
+
 int main()
 {
     int n;
     cout << "Introduce el numero de digitos: " << flush;
     cin >> n;
+    vector<string> v;
+    inicializar(v, n);
+    mostrar(v);
+    // Medir tiempo de aqui
+    radix(v);
+    // a aqui
+    mostrar(v);
+
+    return 0;
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
