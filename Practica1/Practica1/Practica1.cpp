bfed41e2d9b56fa3e1fd08aa93b94688b54719ef
nestor <nestormg98@gmail.com>
2020-02-25 23:04:57 +0100

    Parece que chuta, quedaria lo de medir tiempos y compararlo

--- Practica1/Practica1/Practica1.cpp
+++ Practica1/Practica1/Practica1.cpp
@@ -3,15 +3,37 @@
 
 #include <iostream>
 #include <vector>
+#include <random>
+#include <math.h>
+#include <string>
 
+// El vector de la cuenta de cada digito se inicializa con -1 en el 0 para que el algoritmo funcione correctamente:
 #define CUENTA_INICIAL {-1,0,0,0,0,0,0,0,0,0}
 
-const int N_ELEMENTOS = 100;
+#define N_ELEMENTOS 100000
 
 using namespace std;
 
-void inicializar(vector<string>& v, const int n) {
+
+// Muestra todos los elementos de v separados por un espacio por salida estandar
+void mostrar(vector<string>& v) {
+    for (auto e : v) {
+        cout << e << " ";
+    }
+    cout << endl;
+}
+
+
+// Rellena los N_ELEMENTOS primeros elementos de v con strings de longitud n_digs que representan numeros del 0 al 10^n_digs-1
+void inicializar(vector<string>& v, const int n_digs) {
+  
     for (int i = 0; i < N_ELEMENTOS; i++) {
+        long num = rand() % long(pow(10.0, n_digs)); // numero aleatorio entre 0 y 10^n_digs, es decir, un numero con n_digs
+        string num_str = to_string(num); // en string
+        while (num_str.length() < n_digs) { // si tiene menos de n_digs digitos, se añaden 0s a la izq
+            num_str = "0" + num_str;
+        }
+        v[i] = num_str;
         // TODO: strings aleatorias de long n
     }
 }
@@ -31,17 +53,22 @@ void radix(vector<string>& v, const int n_dig) {
     vector<int> cuenta = CUENTA_INICIAL; // lleva la cuenta del numero de veces que aparece cada digito
     vector<string> aux(v.size()); // auxiliar del mismo tamaño que el original
     for (int i = n_dig - 1; i >= 0; i--) {
+        // Si se quieren ver la evolucion con las iteraciones, descomentar estas tres lineas:
+            //cout << "------ iter " << n_dig - i << " ------\n";
+            //mostrar(v);
+            //cout << "--------------------\n";
         // Para cada digito de los numeros en v:
         for (auto e : v) {
             int digito = e[i] - '0'; // i-ésimo digito
+            //cout << "digito: " << digito << endl;
             cuenta[digito]++; // Contamos el digito correspondiente
         }
         // Acumulamos las cuentas de los digitos de izquierda a dcha:
         acumular(cuenta);
         // Y recolocamos cada elemento del original en el auxiliar según la cuenta:
-        for (int j = N_ELEMENTOS - 1; i >= 0; i--) { // (empezando por la dcha)
+        for (int j = N_ELEMENTOS - 1; j >= 0; j--) { // (empezando por la dcha)
             string elemento = v[j]; // Tomamos el j-esimo elemento
-            int digito = elemento[i] - '0'; // Y su i-esimo digito 
+            int digito = elemento[i] - '0'; // Y su i-esimo digito
             int indice = cuenta[digito]--; // Posicion en el auxiliar (y reducimos esa cuenta)
             aux[indice] = elemento;
         }
@@ -49,8 +76,10 @@ void radix(vector<string>& v, const int n_dig) {
         v = aux; // Comprobar que esto funcione, tengo mis dudas
         // Y resetear la cuenta:
         cuenta = CUENTA_INICIAL;
-    }    
+    }
 }
+
+
 
 // v el vector de numeros (formato string), n el numero de digitos
 // ordena v con el algoritmo de radix LSF
@@ -58,6 +87,7 @@ void radix_rec(vector<string>& v, const int n, const int j) {
     // Iterar desde el digito menos significativo
     vector<int> cuenta = CUENTA_INICIAL; // lleva la cuenta del numero de veces que aparece cada digito
     for (int i = n - 1; i >= 0; i--) {
+       
         // Para cada digito de los numeros en v:
         for (auto e : v) {
             int digito = e[i] - '0'; // i-ésimo digito
@@ -70,24 +100,21 @@ void radix_rec(vector<string>& v, const int n, const int j) {
 }
 
 
-void mostrar(vector<string>& v) {
-    for (auto e : v) {
-        cout << e << " ";
-    }
-    cout << endl;
-}
 
 int main()
 {
-    int n;
+    int n_digs;
     cout << "Introduce el numero de digitos: " << flush;
-    cin >> n;
-    vector<string> v;
-    inicializar(v, n);
+    cin >> n_digs; 
+    vector<string> v(N_ELEMENTOS); // Se inicializa con N_ELEMENTOS
+    inicializar(v, n_digs); // Valores aleatorios de n_digs cada uno
+    cout << "Inicial:" << endl;
     mostrar(v);
+    cout << "-----------------------------------------------" << endl;
     // Medir tiempo de aqui
-    radix(v);
+    radix(v,n_digs); // Se ordena
     // a aqui
+    cout << "Ordenado:" << endl;
     mostrar(v);
 
     return 0;
