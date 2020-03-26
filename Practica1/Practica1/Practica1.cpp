511164d140386166151d7c79117eadfa679aba28
Andrew <aog25.2014@gmail.com>
2020-03-26 18:26:26 +0100

    Makefile, medicion de tiempo y std::sort

--- Practica1/Practica1/Practica1.cpp
+++ Practica1/Practica1/Practica1.cpp
@@ -6,11 +6,17 @@
 #include <random>
 #include <math.h>
 #include <string>
+#include <ctime>
+#include <algorithm>
+#include <ratio>
+#include <chrono>
 
 // El vector de la cuenta de cada digito se inicializa con -1 en el 0 para que el algoritmo funcione correctamente:
 #define CUENTA_INICIAL {-1,0,0,0,0,0,0,0,0,0}
 // Numero de elementos del vector:
 #define N_ELEMENTOS 100000
+
+#define hrc std::chrono::high_resolution_clock
 
 using namespace std;
 
@@ -25,7 +31,7 @@ void mostrar(const vector<string>& v) {
 // Rellena los n_eltos primeros elementos de v con strings de longitud n_digs que representan numeros del 0 al 10^n_digs-1
 void inicializar(vector<string>& v, const int n_digs) {
     int n_eltos = v.size();
-    for (int i = 0; i < n_eltos; i++) {
+   /* for (int i = 0; i < n_eltos; i++) {
         long num = rand() % long(pow(10.0, n_digs)); // numero aleatorio entre 0 y 10^n_digs, es decir, un numero con n_digs
         // Nota: parece que para valores de n_digs>7, casi todos tienen menos digitos... Probar otra f que no sea rand?
         // long no lo ha funcionado
@@ -34,6 +40,12 @@ void inicializar(vector<string>& v, const int n_digs) {
             num_str = "0" + num_str;
         }
         v[i] = num_str;
+    }*/
+    for (int i = 0; i < n_eltos; i++){
+        for(int j = 0; j < n_digs; j++){
+            char num = (rand() % 10) + '0';
+            v[i] += num;
+        }
     }
 }
 
@@ -82,40 +94,88 @@ void radix(vector<string>& v, const int n_dig) {
     }
 }
 
-// v el vector de numeros (formato string), n el numero de digitos
+// v el vector de numeros (formato string), n_dig el numero de digitos
 // ordena v con el algoritmo de radix LSF
-void radix_rec(vector<string>& v, const int n, const int j) {
-    // Iterar desde el digito menos significativo
+void radix_rec(vector<string>& v, const int n_dig) {
     vector<int> cuenta = CUENTA_INICIAL; // lleva la cuenta del numero de veces que aparece cada digito
-    for (int i = n - 1; i >= 0; i--) {
-       
+    int n_eltos = v.size();
+    vector<string> aux(n_eltos); // auxiliar del mismo tamaño que el original
+    // Iterar desde el digito menos significativo
+    for (int i = n_dig - 1; i >= 0; i--) {
+        // Si se quiere ver la evolucion con las iteraciones, descomentar estas tres lineas:
+            //cout << "------ iter " << n_dig - i << " ------\n";
+            //mostrar(v);
+            //cout << "--------------------\n";
         // Para cada digito de los numeros en v:
         for (auto e : v) {
             int digito = e[i] - '0'; // i-ésimo digito
+            //cout << "digito: " << digito << endl;
             cuenta[digito]++; // Contamos el digito correspondiente
-
         }
+        // Acumulamos las cuentas de los digitos de izquierda a dcha:
+        acumular(cuenta);
+        // Y recolocamos cada elemento del original en el auxiliar según la cuenta:
+        for (int j = n_eltos - 1; j >= 0; j--) { // (empezando por la dcha)
+            string elemento = v[j]; // Tomamos el j-esimo elemento
+            int digito = elemento[i] - '0'; // Y su i-esimo digito
+            int indice = cuenta[digito]--; // Posicion en el auxiliar (y reducimos esa cuenta)
+            aux[indice] = elemento;
+        }
+        // Finalmente, actualizamos el vector original:
+        radix(aux);    // Comprobar que esto funcione, tengo mis dudas -- si que funciona
+                    // Idea para eficiencia (si merece la pena depende de como de rapida sea la operacion v=aux): 
+                    // en la siguiente iteracion, tomar aux como original y v como auxiliar, 
+                    // con esto, te ahorras la mitad de las copias 
+
+        // Y resetemos el vector de cuenta:
         cuenta = CUENTA_INICIAL;
     }
 }
 
+
+
+
 int main()
 {
     int n_digs, n_eltos;
+    hrc::time_point t1, t2;
+
     cout << "Introduce el numero de digitos: " << flush;
     cin >> n_digs; 
     cout << "Y el numero de elementos a ordenar: " << flush;
     cin >> n_eltos;
+
     vector<string> v(n_eltos); // Se inicializa con n_eltos
     inicializar(v, n_digs); // Valores aleatorios de n_digs cada uno
+
+    vector<string> v_aux(v);
+    //v_aux = v;
+
+    t1 = hrc::now();
+    sort(v_aux.begin(), v_aux.end());
+    t2 = hrc::now();
+    chrono::duration<double> tiempo_sort = chrono::duration_cast<chrono::duration<double>>(t2 - t1);
+
     cout << "Inicial:" << endl;
     mostrar(v);
     cout << "-----------------------------------------------" << endl;
-    // Medir tiempo de aqui
+
+
+    t1 = hrc::now();
     radix(v,n_digs); // Se ordena
-    // a aqui
+    t2 = hrc::now();
+    chrono::duration<double> tiempo_radix = chrono::duration_cast<chrono::duration<double>>(t2 - t1);
+
+
     cout << "Ordenado:" << endl;
     mostrar(v);
+
+    cout << "Ordenado2:" << endl;
+    mostrar(v_aux);
+
+    cout << "Tiempo Radix: " << tiempo_radix.count() << endl;
+    cout << "Tiempo Sort: " << tiempo_sort.count() << endl;
+
 
     return 0;
 }
