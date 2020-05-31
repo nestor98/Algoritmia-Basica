dd50d9ee2470d2a8c7a92e061c1c2c2687270a36
andrewknoll <aog25.2014@gmail.com>
2020-05-31 21:45:02 +0200

    Comentado de fuerza_bruta.py

--- Practica2/fuerza_bruta.py
+++ Practica2/fuerza_bruta.py
@@ -12,17 +12,29 @@ def distancia(pos0, posf):
 
 
 def expandir(nivel, pos0, posPadre, lista, n_minas, resultado):
-    # print("Expandiendo nodo " + str(nivel) + " " + str(lista) + " " + str(pos0) + "\n")
+    """
+    Para un escenario con n_minas y cuya posición inicial de Nikita sea pos0:
+    calcula el coste de todos los hijos de un nodo con posición "posPadre" en el plano del escenario,
+    (los cuales forman parte del nivel "nivel" de un árbol completo, cuya raíz posee
+    "pos0" como posición) y para el cual "lista" es la lista de minas sin visitar en el
+    recorrido desde el nodo raíz hasta el nodo actual de dicho árbol.
+    "resultado" es el coste de recorrido mínimo encontrado desde que se comenzó a expandir el primer nodo
+    """
     if nivel == n_minas:
-        # no expandir más
+        # no expandir más, pues ya se han recorrido todas las minas y devolver la distancia de vuelta
         return distancia(posPadre, pos0)
     else:
+        # para cada mina restante
         for j, _ in enumerate(lista):
+            # creamos una lista sin la mina que vamos a visitar para pasárselo a la siguiente iteración recursiva
             lista_restantes = deepcopy(lista)
             lista_restantes.pop(j)
+            # expandir hijos del nodo generado (en el siguiente nivel, con la lista de minas restantes)
+            # y sumar la distancia del nodo generado
             coste = expandir(
                 nivel + 1, pos0, lista[j], lista_restantes, n_minas, resultado
             ) + distancia(posPadre, lista[j])
+            # devolver solo el coste del recorrido menor que se haya encontrado
             if coste < resultado:
                 resultado = coste
         return resultado
@@ -31,21 +43,24 @@ def expandir(nivel, pos0, posPadre, lista, n_minas, resultado):
 if __name__ == "__main__":
 
     f = open(sys.argv[1], "r")  # leer archivo
-    f_out = open(sys.argv[2], "w")
+    f_out = open(sys.argv[2], "w")  # archivo de salida
 
     n = int(f.readline().replace("\n", ""))  # leer el numero de escenarios
     escenarios = []
 
+    # lee todos los escenarios
     for i in range(n):
         # print(i)
         escenarios.append(leer_escenario(f))
 
     for e in escenarios:
+        # expandir el árbol desde el nivel 0, con la posición inicial, con toda la lista de minas y con coste inicial infinito
         start = timer()
         upperBound = expandir(
             0, e.pos_ini, e.pos_ini, e.lista_minas, e.n_minas, math.inf
         )
         end = timer()
+        # mostrar resultado
         f_out.write(str(upperBound) + " " + str(end - start) + "\n")
         f_out.flush()
 
